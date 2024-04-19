6bc6d8f23bb8a9b5c4948902a34d8b6e04671489
Brandon Ortiz <ortizbrandon140@gmail.com>
2024-04-19 13:46:00 -0500

    Graphing progress kinda mid tho

--- graphing.jl
+++ graphing.jl
@@ -2,3 +2,19 @@ import Pkg
 Pkg.add("Plots")
 Pkg.pkg"add Plots#master"
 
+using Plots
+
+# Array{Int64,1}
+
+struct Plotter 
+    x_vals::Vector{Float64}
+    y_vals::Vector{Float64}
+end
+
+function plotGraph(input::Plotter) 
+    gui(plot(input.x_vals, input.y_vals))
+end 
+
+plt::Plotter = Plotter([0.0,100.0], [3.0, 80.0])
+
+plotGraph(plt)
