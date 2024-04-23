105c571f7ffc450d24fdc5155aa6502d82c45566
lseelig <37001137+LSeelig@users.noreply.github.com>
2024-04-22 22:31:22 -0500

    Rewrite parsing to implement implicit mult (unfinished)

--- main.jl
+++ main.jl
@@ -1,77 +1,99 @@
-include("./graphing.jl")
+# comment for quicker testing
+# include("./graphing.jl")
 
-const base = 2.0
-const len = 8.0
-const is_round = true
-const digits = 10
+# Globals
 const exits = Set{String}(["exit", "exit()", "quit", "quit()", "end"])
-const step = len / 128
-prev = 0.0
+graph = false
+last::Float64 = 0
 
-# reset per-run variables
-function reset()
-  global graph = false
-  global var_x = -len
+struct Graphing
+    length::Float64
+    steps::Int64
 end
 
-reset()
-
-abstract type Expression end
-
-struct Constant <: Expression
-  value::Float64
+struct Rounding
+    enableRound::Bool
+    digits::Int8
 end
 
-struct Variable <: Expression
-  value::Char
+struct Settings
+    base::Int8
+    logbase::Float64
+    angles::String
+    rounding::Rounding
+    graphing::Graphing
 end
 
-struct OpExpression <: Expression
-  left::Expression
-  op::Function
-  right::Expression
-end
-
-struct GroupExpression <: Expression
-  group::Expression
-end
-
-struct FunExpression <: Expression
-  funct::Function
-  group::Expression
-end
-
-struct EmptyExpression <: Expression end
-
-const precedence::Dict{Char, Int8} = Dict(
-  '(' => 4,
-  '^' => 3,
-  '%' => 2,
-  '*' => 2,
-  '/' => 2,
-  '+' => 1,
-  '-' => 1
+# Default Settings
+const settings = Settings(
+    Int8(10),
+    2.0,
+    "radians",
+    Rounding(
+        true,
+        Int8(10)
+    ),
+    Graphing(
+        8.0,
+        128
+    )
 )
 
-const operation::Dict{Char, Function} = Dict(
-  '*' => function mult(x,y) return x*y end,
-  '/' => function div(x,y) return x/y end,
-  '+' => function add(x,y) return x+y end,
-  '-' => function sub(x,y) return x-y end,
-  '%' => function mod(x,y) return x%y end,
-  '^' => function pow(x,y) return x^y end
+const step::Float64 = settings.graphing.length / settings.graphing.steps
+
+struct Operator
+    op::Function
+    lvl::Int8
+end
+
+# Not including composition (for internal use only)
+const operators::Dict{Char, Operator} = Dict(
+  '+' => Operator(function add(x,y) return x+y end, 1),
+  '-' => Operator(function sub(x,y) return x-y end, 1),
+  '*' => Operator(function mult(x,y) return x*y end, 2),
+  '/' => Operator(function div(x,y) return x/y end, 2),
+  '%' => Operator(function mod(x,y) return x%y end, 2),
+  '^' => Operator(function pow(x,y) return x^y end, 3)
 )
 
-const constant::Dict{String, Float64} = Dict(
+abstract type Term end
+
+mutable struct Head <: Term
+    next::Union{Term,Nothing}
+end
+
+mutable struct Expression
+    tail::Term
+    levels::Dict{Int8, Array{Term}}
+end
+
+mutable struct Constant <: Term
+    value::Float64
+    op::Function
+    next::Union{Term,Nothing}
+end
+
+const constants::Dict{String, Float64} = Dict(
   "pi" => π,
   "π" => π,
   "e" => ℯ,
   "ℯ" => ℯ,
-  "prev" => prev,
-  "ans" => prev
+  "prev" => last,
+  "last" => last,
+  "ans" => last
 )
 
-const funct::Dict{String, Function} = Dict(
+mutable struct Variable <: Term
+    op::Function
+    next::Union{Term,Nothing}
+end
+
+mutable struct FunctionTerm <: Term
+    funct::Function
+    next::Union{Term,Nothing}
+end
+
+const functions::Dict{String, Function} = Dict(
   # trig functions
   "sin" => x -> sin(x),
   "sine" => x -> sin(x),
@@ -110,212 +132,164 @@ const funct::Dict{String, Function} = Dict(
   "cbrt" => x -> cbrt(x)
 )
 
-# this is dumb
-const funcToPrecedence::Dict{Function, Int8} = Dict(
-  pow => 3,
-  mult => 2,
-  div => 2,
-  mod => 2,
-  add => 1,
-  sub => 1
-)
-
-# returns index of operation with higher precedence, returns -1 if there are no operations
-function lowestPrecedence(ex_str::String)
-  lowestP = 100
-  index = -1
-  for i in range(length(ex_str), 1, step=-1)
-    c = ex_str[i]
-    c_precedence = get(precedence, c, 100)
-    if c_precedence < lowestP
-      lowestP = c_precedence
-      index = i
-    end
-  end
-  return index
+mutable struct Group <: Term
+    expr::Expression
+    op::Function
+    next::Union{Term,Nothing}
 end
 
-# replace leftmost EmptyExpression with group
-function attachGroup(group::Expression, other_ex::OpExpression, right=false)
-  if right
-    recur = attachGroup(group, other_ex.right)
-    return OpExpression(other_ex.left, other_ex.op, recur)
-  else
-    recur = attachGroup(group, other_ex.left)
-    return OpExpression(recur, other_ex.op, other_ex.right)
-  end
+function newExpr()
+    return Expression(Head(nothing),Dict())
 end
 
-function attachGroup(group::Expression, other_ex::EmptyExpression)
-  return group
-end
+last_graph::Expression = newExpr()
 
-function sandwichGroup(group::Expression, left::OpExpression, right::OpExpression)
-  left_leaf = left
-  right_leaf = right
-  while left_leaf.right != EmptyExpression()
-    try 
-      left_leaf = left_leaf.right
-    catch
-      throw("Expected an OpExpression but got: $left_leaf")
-    end
+function parseInput(input::AbstractString)
     
-  end
-  while right_leaf.left != EmptyExpression()
-    try 
-      right_leaf = right_leaf.left
-    catch
-      throw("Expected an OpExpression but got: $right_leaf", )
-    end
-  end
-  if funcToPrecedence[left_leaf.op] > funcToPrecedence[right_leaf.op]
-    return attachGroup(attachGroup(group, left, true), right)
-  else
-    return attachGroup(attachGroup(group, right), left, true)
-  end
-end
+    # Create parsed expression (eventual retval)
+    expr = newExpr()
 
-function sandwichGroup(group::Expression, left::EmptyExpression, right::EmptyExpression)
-  return group
-end
-
-function sandwichGroup(group::Expression, left::OpExpression, right::EmptyExpression)
-  return attachGroup(group, left, true)
-end
-
-function sandwichGroup(group::Expression, left::EmptyExpression, right::OpExpression)
-  return attachGroup(group, right)
-end
-
-# turns string into series of Expressions
-function parseExpression(ex_str::String)
-  ex_str = String(strip(ex_str))
-  if ex_str == ""
-    return EmptyExpression()
-  end
-  # grouping
-  open = findfirst(==('('), ex_str)
-  if !isnothing(open)
-    num_open = 1
-    close = open
-    while num_open > 0 && close < length(ex_str)
-      close += 1
-      if ex_str[close] == '('
-        num_open += 1
-      end
-      if ex_str[close] == ')'
-        num_open -= 1
-      end
-    end
-    if num_open > 0
-      throw("No matching close parenthesis found")
-    end
-
-    left = EmptyExpression()
-    right = EmptyExpression()
-    inside = GroupExpression(parseExpression(ex_str[open+1:close-1]))
-    if open > 1
-      end_left = open - 1
-      while ex_str[end_left] == ' ' end_left -= 1 end
-      start_funct = end_left
-      last_funct = start_funct
-      is_funct = false
-      while start_funct != 0
-        if haskey(funct, ex_str[start_funct: end_left])
-          inside = FunExpression(funct[ex_str[start_funct:end_left]], inside)
-          is_funct = true
-          last_funct = start_funct
+    # iter through string
+    index = 1
+    len = length(input)
+    while index <= len
+        # strip spaces in front of input
+        if input[index] == ' '
+            index += 1
+            continue
         end
-        start_funct -= 1
-      end
-      if is_funct end_left = last_funct - 1 end
-      if end_left > 0
-        left = parseExpression(ex_str[1:end_left])
-      end
-    end  
+        start = index
+        # parse negative
+        if input[index] == '-'
+            expr.tail.next = Constant(-1, div(x,y), nothing)
+            index += 1
+        # parse variables
+        elseif input[index] == 'x'
+            global graph = true
+            expr.tail.next = Variable(x->x, nothing)
+            index += 1
+        # parse parentheses
+        elseif input[index] == '('
+            open = 1
+            close = 0
+            index += 1
+            # check if valid
+            while open > close && index <= len
+              if input[index] == '(' open += 1 
+              elseif input[index] == ')' close += 1 end
+              index += 1
+            end
+            if (open != close) throw("Couldn't parse "*input[start:index]*": bad parentheses") end
+            # parse inside, add to expression
+            if index - start == 1 throw("Couldn't parse "*input[start:index]*": empty parentheses")
+            else expr.tail.next = Group(parseInput(input[start+1:index-2]), x->x, nothing) end
+        else
+            # parse digits
+            while index <= len && (isdigit(input[index]) || input[index] == '.') index += 1 end
+            if start != index
+                try  
+                    parse(Float64, input[start:index-1])
+                catch
+                    throw("Couldn't parse "*input[start:index-1]*": bad digits")
+                end
+                expr.tail.next = Constant(parse(Float64, input[start:index-1]), x->x, nothing)
+            # parse constants and functions
+            else
+                while index<=len && !(isdigit(input[index])||input[index]=='.'||input[index]=='(') index+=1 end
+                if haskey(constants, input[start:index-1])
+                    expr.tail.next = Constant(constants[input[start:index-1]], x->x, nothing)
+                elseif haskey(functions, input[start:index-1])
+                    expr.tail.next = FunctionTerm(functions[input[start:index-1]], nothing)
+                    expr.tail = expr.tail.next
+                    # hardcoded - insert function term into precedence (levels) dict
+                    if (haskey(expr.levels, 4))
+                        expr.levels[4][2].prev = expr.tail
+                        expr.levels[4][2] = expr.tail
+                    else expr.levels[4] = [Head(expr.tail), expr.tail] end
+                    continue
+                else throw("Couldn't parse "*input[start:index-1]*": no matching constant or function.") end
+            end
+        end
+        expr.tail = expr.tail.next
+        
+        # Parse operation (function operation is continue so it skips)
+        # Remove spaces
+        while index <= len && input[index] == ' '
+            index += 1
+            continue
+        end
+        # Return if end
+        if index > len
+            return expr
+        end
 
-    if close < length(ex_str)
-      right = parseExpression(ex_str[close+1:length(ex_str)])
+        if haskey(operators, input[index])
+            expr.tail.op = operators[input[index]].op
+            if haskey(expr.levels, operators[input[index]].lvl)
+                expr.levels[operators[input[index]].lvl][2].prev = expr.tail
+                expr.levels[operators[input[index]].lvl][2] = expr.tail
+            else
+                expr.levels[operators[input[index]].lvl] = Term[Head(expr.tail), expr.tail]
+            end
+            index += 1
+        elseif index < len
+            expr.tail.op = (x,y) -> x*y
+            if haskey(expr.levels, 2)
+                expr.levels[2][2].prev = expr.tail
+                expr.levels[2][2] = expr.tail
+            else expr.levels[2] = [Head(expr.tail), expr.tail] end
+        end
     end
-
-    return sandwichGroup(inside, left, right)
-  end
-
-  lowestPIndex = lowestPrecedence(ex_str)
-  # no operations
-  if lowestPIndex < 0
-    try
-      if ex_str == "x"
-        global graph = true
-        return Variable('x')
-      end
-      if haskey(constant, ex_str)
-        return Constant(constant[ex_str])
-      end
-      value = parse(Float64, ex_str)
-      return Constant(value)
-    catch
-      # need to support variables too
-      throw("Non-operation characters must be numbers.")
-    end
-  end
-
-  op = ex_str[lowestPIndex]
-  ex_op = operation[op]
-  left = parseExpression(ex_str[1:lowestPIndex-1])
-  right = parseExpression(ex_str[lowestPIndex+1:length(ex_str)])
-  return OpExpression(left, ex_op, right)
+    # Should not be reachable because if index > len return expr
+    throw("Control flow error: parsing exited without returning parsed expression.")
 end
-
-# simplifies series of Expressions
-function solveExpression(ex::OpExpression, x::Float64) return ex.op(solveExpression(ex.left, x), solveExpression(ex.right, x)) end
-function solveExpression(ex::GroupExpression, x::Float64) return solveExpression(ex.group, x) end
-function solveExpression(ex::FunExpression, x::Float64) return ex.funct(solveExpression(ex.group, x)) end
-function solveExpression(ex::EmptyExpression, x::Float64) return 0 end
-function solveExpression(ex::Constant, x::Float64) return ex.value end
-function solveExpression(ex::Variable, x::Float64) return x end
 
 function loop()
-  print("Expression: ")
-  expression = readline()
+    print("Expression: ")
+    expression = readline()
 
-  # pre-parsing, unnecessary if parseExpression called recursively
-  if in(expression, exits)
-    exit()
-  end
-  # check if carrying previous answer ("" check is clunky but more efficient than checking starting operation in recursive)
-  stripped_exp = lstrip(expression)
-  if stripped_exp != "" && haskey(operation, stripped_exp[1])
-    parsed_ex = parseExpression("(" * string(prev) * ")" * expression)
-  else
-    parsed_ex = parseExpression(expression)
-  end
-
-  # for debug
-  # println(parsed_ex)
-
-  # for graphed equations
-  if graph
-    x_vals = Vector{Float64}([])
-    y_vals = Vector{Float64}([])
-    while var_x <= len
-      push!(x_vals, var_x)
-      push!(y_vals, solveExpression(parsed_ex,var_x))
-      global var_x += step 
+    # pre-parsing, unnecessary if parseInput called recursively
+    if in(expression, exits)
+        exit()
     end
-    plotGraph(Plotter(x_vals,y_vals))
-    println("See graph")
-  else
-    # for expressions, print integers properly, round floats to conceal error
-    solution = solveExpression(parsed_ex, 0.0)
-    if is_round solution = round(solution, digits = digits) end
-    if isinteger(solution)
-      println(Int64(solution))
-    else println(solution) end
-    global prev = solution
-  end
-  reset()
-  loop()
-end
+    # check if carrying previous answer ("" check is clunky but more efficient than checking starting operation in recursive)
+    stripped_exp = lstrip(expression)
+    if stripped_exp != "" && haskey(operators, stripped_exp[1])
+        if length(stripped_exp) > 1 && stripped_exp[2] != ' ' && stripped_exp[1] == '-'
+        parsed_ex = parseInput("0" * expression)
+        else parsed_ex = parseInput("(" * string(prev) * ")" * expression) end
+    else parsed_ex = parseInput(expression) end
 
+    # for debug
+    println(parsed_ex)
+    
+    #=
+    # for graphed equations
+    if graph
+        global last_graph = parsed_ex
+        x_vals = Vector{Float64}([])
+        y_vals = Vector{Float64}([])
+        while var_x <= len
+        push!(x_vals, var_x)
+        push!(y_vals, solveExpression(parsed_ex,var_x))
+        global var_x += step 
+        end
+        plotGraph(Plotter(x_vals,y_vals))
+        println("See graph")
+    else
+        # for expressions, print integers properly, round floats to conceal error
+        solution = solveExpression(parsed_ex, 0.0)
+        global last = solution
+        if settings.rounding.enableRound solution = round(solution, digits = digits) end
+        if isinteger(solution)
+        println(Int64(solution))
+        else println(solution) end
+    end
+    =#
+    
+    global graph = false
+    
+    loop()
+end
+  
 loop()
